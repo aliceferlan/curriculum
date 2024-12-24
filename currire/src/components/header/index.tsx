104bb222b3a7e847f8083abe78b10696ea115d62
alice <aliceundteles@gmail.com>
2024-12-24 14:47:56 +0900

    ヘッダとレイアウトの作成

---
+++ currire/src/components/header/index.tsx
@@ -0,0 +1,25 @@
+import Link from "next/link";
+
+const Header = () => {
+	return (
+		<div className="navbar bg-base-100">
+			<div className="flex-1">
+				<Link className="btn btn-ghost text-xl" href={"/"}>
+					職務経歴書
+				</Link>
+			</div>
+			<div className="flex-none">
+				<ul className="menu menu-horizontal px-1">
+					<li>
+						<Link href={"/carry"}>職務経歴</Link>
+					</li>
+					<li>
+						<Link href={"/history"}>履歴</Link>
+					</li>
+				</ul>
+			</div>
+		</div>
+	);
+};
+
+export default Header;
