5970489029639eb4a0e02228db8e19f34a7675a7
alice <aliceundteles@gmail.com>
2024-12-26 08:29:23 +0900

    未使用インポートののコメントアウト

--- currire/src/app/layout.tsx
+++ currire/src/app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Header from "@/components/header";
-import { ThemeProvider } from "next-themes";
+// import { ThemeProvider } from "next-themes";
 // import Footer from "@/components/layout/Footer";
 
 const geistSans = Geist({
